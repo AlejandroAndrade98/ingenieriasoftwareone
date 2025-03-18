e2d5793404ca6c4c56f1618a9f75bead0809829b
Alejandro Anibal Andrade <alejandro21112@hotmail.com>
2025-03-17 21:46:17 -0500

    firt commit

---
+++ src/components/EditarEmpleado.tsx
@@ -0,0 +1,8 @@
+export function EditarEmpleado() { 
+    return (
+        <div>
+            <h1>Lista</h1>
+            <p>Esta es la lista de Editar Empleado</p>
+        </div>
+    );
+}
---
+++ src/components/Lista.tsx
@@ -0,0 +1,8 @@
+export function Lista() { 
+    return (
+        <div>
+            <h1>Lista</h1>
+            <p>Esta es la lista de elementos</p>
+        </div>
+    );
+}
